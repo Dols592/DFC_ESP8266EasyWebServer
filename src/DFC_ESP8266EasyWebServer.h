05ecff8e314abf282413bb3aad81c6a81868b0fd
Richard Dols <github@dols.nu>
2018-01-09 19:32:22 +0100

    development

--- src/DFC_ESP8266EasyWebServer.h
+++ src/DFC_ESP8266EasyWebServer.h
@@ -3,11 +3,13 @@
 #define DFC_ESP8266_EASY_WEB_SERVER_H
 
 //Config defines
+#define WEB_TCP_PORT        80
 
 //Includes
 #include "DFC_ESP8266EasyWebServerData.h"
+#include <ESP8266WebServer.h>
 
-class DFC_ESP8266EasyWebServer  
+class DFC_ESP8266EasyWebServer
 {
 public: //Constructor
   DFC_ESP8266EasyWebServer();
@@ -16,11 +18,19 @@ public: //Interface
   void      Init();
   void      Loop();
 
+public: //Static callbacks
+  static void OnPageNotFound();
+  
+  static bool DoPageFromFile();
+  
 protected: //Help functions
+  static String Help_getContentType(String filename);
 
 public: //Config
 
 protected: //Variables
+  static ESP8266WebServer mWebServer;
+
 };
 
 #endif //DFC_ESP8266_EASY_WEB_SERVER_H
